04f051fe50f893e8950343939a8a7633fd7ee2fd
Suhan Wijaya <suhanw@gmail.com>
2024-12-23 19:50:54 +0800

    Add profile reordering feature

--- app/src/components/ProfileList/index.js
+++ app/src/components/ProfileList/index.js
@@ -2,11 +2,27 @@ import React from "react";
 import cn from "classnames";
 
 import style from "./style";
-import { useGetAllProfiles, useSetSelectedProfileId } from "store/profiles";
+import {
+  useGetAllProfiles,
+  useGetSelectedProfile,
+  useSetSelectedProfileId,
+  useUpdateProfileOrder,
+} from "store/profiles";
+
+// 2. Clicking a profile should select the profile and will be highlighted in green and the right panel will be updated.
+// 3. Move up, move down, and add (+) icon are always shown. If the selected profile is at the most top of the list, move up will be disabled. Same goes if the selected profile is at the most bottom of the list, move down will be disabled.
+// 4. User has 4 default profiles: Default, Game, Movie and Music. Default profiles have their own icons, can be moved up and down, but cannot be renamed or deleted. Hence delete and rename icon should not be shown on the UI if the selected profile is a default profile.
 
 function ProfileList() {
   const { allProfiles } = useGetAllProfiles();
-  const { selectedProfileId, setSelectedProfileId } = useSetSelectedProfileId();
+  const { selectedProfileId, selectedProfile } = useGetSelectedProfile();
+  const { setSelectedProfileId } = useSetSelectedProfileId();
+  const {
+    firstProfileSelected,
+    lastProfileSelected,
+    moveProfileUp,
+    moveProfileDown,
+  } = useUpdateProfileOrder();
 
   return (
     <nav className={style.layout}>
@@ -30,12 +46,18 @@ function ProfileList() {
         <div className={style.profileListToolbar}>
           <div className={style.reorderIcons}>
             <button
-              className={style.toolbarIcon}
+              className={cn(style.toolbarIcon, {
+                [style.disabled]: firstProfileSelected,
+              })}
               style={{ backgroundImage: "url(/images/icon_arrow_up.svg)" }}
+              onClick={moveProfileUp}
             />
             <button
-              className={style.toolbarIcon}
+              className={cn(style.toolbarIcon, {
+                [style.disabled]: lastProfileSelected,
+              })}
               style={{ backgroundImage: "url(/images/icon_arrow_down.svg)" }}
+              onClick={moveProfileDown}
             />
           </div>
           <div className={style.crudIcons}>
--- app/src/store/profiles/index.js
+++ app/src/store/profiles/index.js
@@ -40,6 +40,11 @@ export const { getAllProfilesSuccess, setSelectedProfileId } =
 
 export const profileReducer = profileSlice.reducer;
 
+
+/**
+ * React hooks to expose state data and update methods
+ */
+
 export const useGetAllProfiles = function () {
   const allProfiles = useSelector((state) => state.profiles.allProfiles);
   return { allProfiles };
@@ -58,13 +63,58 @@ export const useGetSelectedProfile = function () {
 };
 
 export const useSetSelectedProfileId = function () {
+  const dispatch = useDispatch();
+  return {
+    setSelectedProfileId: (profileId) =>
+      dispatch(setSelectedProfileId(profileId)),
+  };
+};
+
+export const useUpdateProfileOrder = function () {
+  const dispatch = useDispatch();
   const selectedProfileId = useSelector(
     (state) => state.profiles.selectedProfileId
   );
-  const dispatch = useDispatch();
+  const allProfiles = useSelector((state) => state.profiles.allProfiles);
+
+  const firstProfileSelected = allProfiles[0]?.id === selectedProfileId;
+  const lastProfileSelected =
+    allProfiles[allProfiles.length - 1]?.id === selectedProfileId;
+
+  const selectedIndex = allProfiles.findIndex(
+    ({ id }) => id === selectedProfileId
+  );
+
+  const moveProfileUp = function () {
+    if (firstProfileSelected) return;
+
+    const selectedProfile = allProfiles[selectedIndex];
+    const previousProfile = allProfiles[selectedIndex - 1];
+
+    const updatedProfiles = [...allProfiles];
+    updatedProfiles[selectedIndex - 1] = selectedProfile;
+    updatedProfiles[selectedIndex] = previousProfile;
+
+    dispatch(getAllProfilesSuccess(updatedProfiles));
+  };
+
+  const moveProfileDown = function () {
+    if (lastProfileSelected) return;
+
+    const selectedProfile = allProfiles[selectedIndex];
+    const nextProfile = allProfiles[selectedIndex + 1];
+
+    const updatedProfiles = [...allProfiles];
+    updatedProfiles[selectedIndex + 1] = selectedProfile;
+    updatedProfiles[selectedIndex] = nextProfile;
+
+    dispatch(getAllProfilesSuccess(updatedProfiles));
+  };
+
   return {
-    selectedProfileId,
-    setSelectedProfileId: (profileId) =>
-      dispatch(setSelectedProfileId(profileId)),
+    firstProfileSelected,
+    lastProfileSelected,
+    moveProfileUp,
+    moveProfileDown,
   };
 };
