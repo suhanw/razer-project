84c2e2099d78a124eb4fda278e0d93c36f33f849
Suhan Wijaya <suhanw@gmail.com>
2024-12-23 10:05:04 +0800

    Set up basic React app

---
+++ app/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+
+module.exports = {
+  entry: path.resolve(__dirname, "./src/index.js"), // entry file for the app
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]", // for CSS Modules
+              },
+            },
+          },
+          "less-loader", // Compiles Less to CSS
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  ...(isDev && {
+    devServer: {
+      static: path.resolve(__dirname, "../assets"),
+      hot: true,
+    },
+  }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+    }),
+  ],
+  devtool: isDev && "source-map",
+};
